b454642b2182f040fe6d6bb5ce89e973aea1f68f
Tan Chu Minh <cmtan@tma.com.vn>
2024-05-29 07:35:06 +0700

    change to ukwest

--- local.tf
+++ local.tf
@@ -1,12 +1,12 @@
 locals {
-    aks = {
-        name                    = "natcom-aks-01"
-        resource-group-name     = "natcom-aks-rg"
-        location                = "southeastasia"
-        vnet-name               = "natcom-aks-vnet"
-        subnet-name             = "natcom-aks-snet"
-        sku                     = "Standard"
-        private-cluster-enabled = false
-        admin_group_object_ids  = ["a5d5db85-021c-436f-83b9-9e2b9070a0df"]
-    }
+  aks = {
+    name                    = "natcom-aks-01"
+    resource-group-name     = "natcom-aks-rg"
+    location                = "ukwest"
+    vnet-name               = "natcom-aks-vnet"
+    subnet-name             = "natcom-aks-snet"
+    sku                     = "Standard"
+    private-cluster-enabled = false
+    admin_group_object_ids  = ["f9230fd6-6a5e-49af-af88-ed85499452ab"]
+  }
 }
